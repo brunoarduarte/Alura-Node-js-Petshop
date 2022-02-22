63f1115d38be28167ba2ca9f455f7c45226206e0
Bruno Duarte <brunoarduarte@gmail.com>
2022-02-22 11:11:01 -0300

    Adjust

--- infrastructure/database/conexao.js
+++ infrastructure/database/conexao.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const conexao = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'bruno',
-  password: 'Glau3005@',
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database: 'agenda_petshop'
 });
 
